712b62b9d10726d795208c40efc4c7ba24dbbd85
Isaías Leibovich <isaias.leibovich@gmail.com>
2023-05-06 18:40:57 -0300

    .

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,11 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './navBar.css';
 
 // SVG
 import { CartIcon } from '../../img/CartIcon';
 import { HamburgerMenu } from '../../img/HamburgerMenu';
+import { BottomArrow } from '../../img/BottomArrow';
 
 export const NavBar = () => {
+    const [mobileProductsActive, SetMPA] = useState("desactivated");
+
+    function changeMPA() {
+        if (mobileProductsActive === "desactivated") {
+            SetMPA("active");
+        } else {
+            SetMPA("desactivated");
+        }
+    }
+
     return (
         <>
             <header>
@@ -42,6 +53,31 @@ export const NavBar = () => {
                     <div id="cartBubble"></div>
                 </div>
             </header>
+
+            <div id="mobileHeader">
+                <div id="linksContainer">
+                    <div className="link">
+                        <a href="/">Inicio</a>
+                    </div>
+                    <div className={`link ${mobileProductsActive}`} onClick={changeMPA}>
+                        <p href="/productos/">Productos <BottomArrow /></p>
+                    </div>
+                    <div id="productOptions" className={mobileProductsActive}>
+                        <div className="option">
+                            <a href="/productos/hoodies/">Hoodies</a>
+                        </div>
+                        <div className="option">
+                            <a href="/productos/remeras/">Remeras</a>
+                        </div>
+                        <div className="option">
+                            <a href="/productos/">Todos</a>
+                        </div>
+                    </div>
+                    <div className="link">
+                        <a href="/contacto/">Contacto</a>
+                    </div>
+                </div>
+            </div>
         </>
     )
 }
---
+++ src/img/BottomArrow.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const BottomArrow = () => {
+    return (
+        <svg viewBox="0 0 24 24" width="24" height="24">
+            <path d="M12 16.5l-7.5-7.5 1.5-1.5L12 13.5l6.75-6.75 1.5 1.5z" fill="#333" />
+        </svg>
+    )
+}
