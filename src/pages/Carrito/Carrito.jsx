9917f35c07e0c6769c1520c41a3b300c4192dc62
Isaías Leibovich <isaias.leibovich@gmail.com>
2023-05-14 16:07:57 -0300

    .

--- src/pages/Carrito/Carrito.jsx
+++ src/pages/Carrito/Carrito.jsx
@@ -11,10 +11,43 @@ const Carrito = () => {
     <>
       <NavBar />
 
+      <div id="finishCart">
+        <button>
+          <p>Finalizar compra</p>
+        </button>
+      </div>
+
       <div id="carritoContainer">
         <h1>Carrito</h1>
 
         <div id="carrito">
+          {
+            productos.map(item => {
+              return (
+                <div className="cartProduct" key={item.id}>
+                  <div className="imgContainer">
+                    <img src={require(`../../img/products/${item.img[0]}`)} alt={`${item.name}-img1`}/>
+                  </div>
+                  <div className='nameContainer'>
+                    <p>{item.name}</p>
+                  </div>
+                  <div className='priceContainer'>
+                    <p>{item.price}</p>
+                  </div>
+                  <div className='deleteContainer'>
+                    <button>
+                      <p>Eliminar</p>
+                    </button>
+                  </div>
+                  <div className="quantityContainer">
+                    <div className="quantity">
+                      <p>1</p>
+                    </div>
+                  </div>
+                </div>
+              )
+            })
+          }
         </div>
       </div>
 
